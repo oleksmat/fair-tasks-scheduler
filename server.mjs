35c8e28bba285660016e24b67900fab12c76f0f5
Oleksandr Matvieiev <me@oleksmat.com>
2024-11-09 00:03:23 +0200

    update naming

--- server.mjs
+++ server.mjs
@@ -2,19 +2,19 @@ import { createServer } from 'node:http';
 
 export class Server {
   constructor(
-    queue
+    tasks
   ) {
     /**
      * @type { import("./tasks.mjs").Tasks }
      */
-    this.queue = queue;
+    this.tasks = tasks;
     this.server = createServer((req, res) => {
       const url = new URL(req.url, 'http://localhost:4000');
 
       const customerId = url.searchParams.get('customerId');
       const duration = Number(url.searchParams.get('duration'));
 
-      this.queue.addTask({ customerId, duration });
+      this.tasks.addTask({ customerId, duration });
 
       res.statusCode = 200;
       res.end();
